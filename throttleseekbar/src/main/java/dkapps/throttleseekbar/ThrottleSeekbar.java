12be53d880d91a1ce92ba9f7fe848c56f6a1c248
David Kim <deedeekay89@gmail.com>
2016-01-26 17:11:53 -0800

    fixed typo that caused error.

--- throttleseekbar/src/main/java/dkapps/throttleseekbar/ThrottleSeekbar.java
+++ throttleseekbar/src/main/java/dkapps/throttleseekbar/ThrottleSeekbar.java
@@ -33,8 +33,8 @@ public class ThrottleSeekbar extends SeekBar {
 
     // Drawables
     LayerDrawable mSeekbarLayerlist;
-    InsetDrawable mLeftBGInset, mRightBGInset, mLeftFGInset, mRightFGInset;
     GradientDrawable mLeftBG, mRightBG, mLeftFG, mRightFG;
+//    InsetDrawable mLeftBGInset, mRightBGInset, mLeftFGInset, mRightFGInset;
 
     Timer timer;
     boolean cancelTimer;
